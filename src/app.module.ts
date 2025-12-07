4efdc3800339714ebe396c9965b0aa3917d3bf72
mohsenmdb <mohsen.mdb@gmail.com>
2025-12-07 19:34:08 +0330

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ProductsModule } from './products/products.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
+
+@Module({
+  imports: [UserModule, ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
+
