954109c1169bcb1218830af6101bb922aa456def
mohsenmdb <mohsen.mdb@gmail.com>
2025-12-08 22:23:41 +0330

    create postgres table

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('user')
+export default class User {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({unique: true, nullable: false})
+    email: string
+
+    @Column({length: 25, nullable: true})
+    firs_name: string
+
+    @Column({length: 25, nullable: true})
+    last_name: string
+
+    @Column({select: false, nullable: false})
+    password: string
+}
